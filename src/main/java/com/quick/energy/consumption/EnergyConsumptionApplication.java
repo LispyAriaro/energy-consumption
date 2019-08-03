f338273673fb53ace9dc8b0f120135cce9022e95
Efe Ariaroo <efeariaroo@gmail.com>
2019-08-04 00:24:46 +0100

    Simple refactor rename

--- src/main/java/com/quick/energy/consumption/EnergyConsumptionApplication.java
+++ src/main/java/com/quick/energy/consumption/EnergyConsumptionApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ThirdApplication {
+public class EnergyConsumptionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ThirdApplication.class, args);
+		SpringApplication.run(EnergyConsumptionApplication.class, args);
 	}
 
 }
--- src/test/java/com/quick/energy/consumption/EnergyConsumptionApplicationTests.java
+++ src/test/java/com/quick/energy/consumption/EnergyConsumptionApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ThirdApplicationTests {
+public class EnergyConsumptionApplicationTests {
 
 	@Test
 	public void contextLoads() {
